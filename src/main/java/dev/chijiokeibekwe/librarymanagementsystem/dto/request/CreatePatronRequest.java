700dc51b84c29d0606b611c5fcf48c98a3df40fb
Chijioke Ibekwe <ibekwe.chijioke18@gmail.com>
2024-05-18 05:59:16 +0100

    feat: validate contact and address fields in create and update patron requests

--- src/main/java/dev/chijiokeibekwe/librarymanagementsystem/dto/request/CreatePatronRequest.java
+++ src/main/java/dev/chijiokeibekwe/librarymanagementsystem/dto/request/CreatePatronRequest.java
@@ -1,5 +1,6 @@
 package dev.chijiokeibekwe.librarymanagementsystem.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record CreatePatronRequest(
@@ -9,9 +10,11 @@ public record CreatePatronRequest(
         @NotNull(message = "Last name is required")
         String lastName,
 
+        @Valid
         @NotNull(message = "Contact is required")
         ContactDetailsRequest contact,
 
+        @Valid
         @NotNull(message = "Address is required")
         AddressRequest address
 )
--- src/main/java/dev/chijiokeibekwe/librarymanagementsystem/dto/request/UpdatePatronRequest.java
+++ src/main/java/dev/chijiokeibekwe/librarymanagementsystem/dto/request/UpdatePatronRequest.java
@@ -1,5 +1,6 @@
 package dev.chijiokeibekwe.librarymanagementsystem.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdatePatronRequest(
@@ -9,9 +10,11 @@ public record UpdatePatronRequest(
         @NotNull(message = "Last name is required")
         String lastName,
 
+        @Valid
         @NotNull(message = "Contact is required")
         ContactDetailsRequest contact,
 
+        @Valid
         @NotNull(message = "Address is required")
         AddressRequest address
 )
